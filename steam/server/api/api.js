14996ff9bc6e33af8a7980fa5112378c8a872cab
Bradley Mize <bmize@co.pierce.wa.us>
2017-02-21 23:32:26 +0000

    Initial steam api setup

--- steam/server/api/api.js
+++ steam/server/api/api.js
@@ -6,6 +6,11 @@ router.get('/', (req, res) => {
   res.send('api works');
 });
 
+router.get('/user/:steamId', (req, res) => {
+  const userGames = require('./games.json');
+  res.send(userGames);
+});
+
 /* Get game info: http://store.steampowered.com/api/appdetails?appids=207140 */
 
 module.exports = router;
